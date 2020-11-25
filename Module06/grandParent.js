875c5eb40e611b08eddf4fccb45a20e7f39a1209
Raghavendragovindula <74422660+Raghavendragovindula@users.noreply.github.com>
2020-11-25 17:49:16 +0530

    Create grandParent.js

---
+++ Module06/grandParent.js
@@ -0,0 +1,20 @@
+import { LightningElement } from 'lwc';
+
+export default class GrandParentDemo extends LightningElement {
+
+    count = 0;
+    
+    handleChildClick(event)
+    {
+        if(event.detail.status == 'Select')
+        {
+            this.count = this.count + 1;
+        }
+        else{
+            this.count = this.count - 1;
+        }
+    }
+    reset() {
+        this.count = 0;
+    }
+}
