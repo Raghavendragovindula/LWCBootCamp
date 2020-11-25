8d7d16c5e31f2851ed4a7062b7dd0cd3be09cfac
Raghavendragovindula <74422660+Raghavendragovindula@users.noreply.github.com>
2020-11-25 17:52:05 +0530

    Create child.js

---
+++ Module06/child.js
@@ -0,0 +1,24 @@
+import { LightningElement, api, track } from 'lwc';
+
+export default class ChildDemo extends LightningElement {
+    @api
+    childTitle;
+    @track
+    bLabel = 'Select';
+    variant = 'success';
+
+    handleClick(event)
+    {
+        this.bLabel = event.target.label == 'Select' ? 'Deselect' : 'Select';
+        this.variant = event.target.label == 'Select' ? 'destructive' : 'success';
+
+        let childEvnt = new CustomEvent('childbtnclick', {
+            bubbles: true,
+            composed: true,
+            detail : {childTitle: this.childTitle, status: event.target.label}
+            
+        });
+
+        this.dispatchEvent(childEvnt);
+    }
+}
