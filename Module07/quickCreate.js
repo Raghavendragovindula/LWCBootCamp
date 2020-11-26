b3a2097cc8adeb767293fdcebea49ca745369047
Raghavendragovindula <74422660+Raghavendragovindula@users.noreply.github.com>
2020-11-26 19:54:36 +0530

    Update quickCreate.js

--- Module07/quickCreate.js
+++ Module07/quickCreate.js
@@ -1,24 +1,22 @@
-import { LightningElement, api } from 'lwc';
-
-import NAME_FIELD from '@salesforce/schema/Contact.FirstName';
-import Account_FIELD from '@salesforce/schema/Contact.AccountId';
-import LAST_NAME from '@salesforce/schema/Contact.LastName';
-
-import ACC_NAME from '@salesforce/schema/Account.Name';
-
-export default class QuickCreate extends LightningElement {
+import { LightningElement } from 'lwc';
+ 
+export default class quickCreate extends LightningElement {
+    strRecordIdAccount;
+    strRecordIdContact;
+    strRecordIdOpportunity;
     
-    fields = [NAME_FIELD, Account_FIELD, LAST_NAME];
-    accFields = [ACC_NAME];
-    handleReset(event) {       
+    arrayFields1 = ['Name'];
+    arrayFields2 = ['Salutation','FirstName','LastName','AccountId'];
+    arrayFields3 = ['Name','StageName','CloseDate','AccountId']
+
+    handleReset(event) {
         const inputFields = this.template.querySelectorAll(
             'lightning-input-field'
         );
         if (inputFields) {
-            inputFields.forEach(field => {                
+            inputFields.forEach(field => {
                 field.reset();
             });
         }
-        
-    }
+     }
 }
